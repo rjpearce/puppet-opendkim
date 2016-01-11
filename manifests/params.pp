41230b34e50fd236cc75126d3ed335b4bd0a4788
Alexey Lapitsky <alexey@spotify.com>
2016-01-11 13:48:01 +0100

    add internalhosts parameter

--- manifests/params.pp
+++ manifests/params.pp
@@ -31,6 +31,10 @@
 #   signatures.
 #   String, default is 'From'
 #
+# [*internalhosts*]
+#   Hosts from which mail should be signed rather than verified.
+#   Array, default is ['127.0.0.1']
+#
 # === Examples
 #
 #  Not called explicitly; referenced by opendkim init and config modules.
@@ -45,6 +49,7 @@ class opendkim::params {
   $syslog_success = false
   $umask  = '002'
   $oversignheaders = 'From'
+  $internalhosts = ['127.0.0.1']
 
   case $::operatingsystem {
     'Ubuntu', 'Debian': {
