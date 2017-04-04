cccfc8f6031d4cb83286758cb7030586de32367f
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2017-04-04 22:45:11 +0000

    allow multiple domains by using KeyTable and SigningTable

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,6 +20,33 @@ class opendkim::config(
       order   => 01;
   }
 
+  concat { '/etc/opendkim/KeyTable':
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+
+  concat::fragment {
+    'opendkim KeyTable header':
+      target  => '/etc/opendkim/KeyTable',
+      source  => 'puppet:///modules/opendkim/etc/opendkim/KeyTable.header',
+      order   => 01;
+  }
+
+  concat { '/etc/opendkim/SigningTable':
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+
+  concat::fragment {
+    'opendkim SigningTable header':
+      target  => '/etc/opendkim/SigningTable',
+      source  => 'puppet:///modules/opendkim/etc/opendkim/SigningTable.header',
+      order   => 01;
+  }
+
+
   if ($::opendkim::params::service_flavor == 'Debian') {
     concat { $::opendkim::params::service_config:
       owner => root,
--- manifests/domain.pp
+++ manifests/domain.pp
@@ -13,13 +13,20 @@ define opendkim::domain(
       source  => $private_key,
       require => Package[$::opendkim::params::package]
   }
-  concat::fragment{ $name:
-    target  => '/etc/opendkim.conf',
-    content => "Domain ${domain}\nKeyFile ${key_folder}/${domain}.key\nSelector ${selector}\n\n",
-    order   => 10,
-    require => File["${key_folder}/${domain}.key"],
-    notify  => Service[$opendkim::params::service];
+
+  concat::fragment { "opendkim KeyTable ${name}":
+      target  => '/etc/opendkim/KeyTable',
+      content  => "${selector}._domainkey.${domain} ${domain}:${selector}:${key_folder}/${domain}.key\n",
+      order   => 10,
+      require => File["${key_folder}/${domain}.key"],
+      notify  => Service[$opendkim::params::service],
+  }
+
+  concat::fragment { "opendkim SigningTable ${name}":
+      target  => '/etc/opendkim/SigningTable',
+      content  => "${domain} ${selector}._domainkey.${domain}\n",
+      order   => 10,
+      notify  => Service[$opendkim::params::service],
   }
 
 }
-
